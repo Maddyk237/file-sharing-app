2d73867e5dde228f313ab670f4807d190d3127d3
Mandar <mkulkarni690@gmail.com>
2025-12-17 09:40:04 +0530

    Baseurl change

--- frontend/frontend/src/api.js
+++ frontend/frontend/src/api.js
@@ -1,4 +1,5 @@
-const API_URL = "http://localhost:5000/api";
+// const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://file-sharing-backend.onrender.com/api";
 
 export const api = async (url, method = "GET", body, isFormData = false) => {
   const token = localStorage.getItem("token");
@@ -7,7 +8,7 @@ export const api = async (url, method = "GET", body, isFormData = false) => {
   if (!isFormData) headers["Content-Type"] = "application/json";
   if (token) headers["Authorization"] = `Bearer ${token}`;
 
-  const res = await fetch(API_URL + url, {
+  const res = await fetch(BASE_URL + url, {
     method,
     headers,
     body: body ? (isFormData ? body : JSON.stringify(body)) : null,
