b13a239c92fcad0fde9ea51382baf0eeaa0aaad9
Mandar <mkulkarni690@gmail.com>
2025-12-17 09:29:10 +0530

    CORS for all origin

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,6 +13,11 @@ const userRoutes = require("./routes/user.routes");
 connectDB();
 
 app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
